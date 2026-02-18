5d8b4f650f0d1ca9ef0139f33318409b95165d14
dan ho <danho@dans-MacBook-Air.local>
2026-02-17 18:08:23 -0800

    implement postlistitem

--- src/app/(protected)/(tabs)/friends.tsx
+++
@@ -1,9 +0,0 @@
-import {View, Text} from 'react-native';
-
-export default function FriendsScreen() {
-  return (
-    <View>
-      <Text>FriendsScreen!</Text>
-    </View>
-  )
-}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,23 @@
 import { DarkTheme, ThemeProvider } from "@react-navigation/native"
 import { Stack } from "expo-router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { StatusBar } from "expo-status-bar"
-
-const queryClient = new QueryClient();
-
-const myTheme = {
-  ...DarkTheme,
-  colors: {
-    ...DarkTheme.colors,
-    primary: 'white',
-  }
-}
 
 export default function RootLayout() {
-  return (
-    <ThemeProvider value={myTheme}>
-      <QueryClientProvider client={queryClient}>
-        <StatusBar style="light" />
-        <Stack screenOptions={{ headerShown: false }} />
-      </QueryClientProvider>
-    </ThemeProvider>
-  )
+    const queryClient = new QueryClient();
+
+    const myTheme = {
+        ...DarkTheme,
+        colors: {
+            ...DarkTheme.colors,
+            primary: 'white',
+        }
+    }
+
+    return (
+        <ThemeProvider value={myTheme}>
+            <QueryClientProvider client={queryClient}>
+                <Stack screenOptions={{ headerShown: false }} />
+            </QueryClientProvider>
+        </ThemeProvider>
+    )
 }
